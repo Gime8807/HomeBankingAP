85ab3d17d473c59ccc0ba3459117442954deb3a7
gimena sasso <94083786+Gime8807@users.noreply.github.com>
2023-08-03 20:51:02 -0300

    first commit

---
+++ src/main/java/com/mindhub/models/Client.java
@@ -0,0 +1,49 @@
+package com.mindhub.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String mail;
+
+    public Client(){}
+
+    public Client(String firstName, String lastName, String mail){
+        this.firstName= firstName;
+        this.lastName= lastName;
+        this.mail= mail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
